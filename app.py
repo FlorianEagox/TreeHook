0d542a43c5e90f8ef2f694380efd449cacf93a67
Seth Painter <timytomy11@gmail.com>
2019-11-12 01:36:15 -0600

    Created a way to store hooks as json and an endpoint to delete them.

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
 import requests
 import json
 import time
 import threading
-import hashlib
+from secrets import token_urlsafe
 
 app = Flask(__name__, subdomain_matching=True)
 app.config['SERVER_NAME'] = 'localhost:443'
 
-hooks = [{
-    "url": "https://webhook.site/49147c97-3619-48b2-bd18-c6f79f584ea4",
-    "delay": 1,
-    "top": False
-}]
+hooks = []
 
 
-def getPage():
+def write_hooks():
+    with open("hooks.json", 'w+') as file:
+        file.write(json.dumps([hook[0] for hook in hooks]))
+
+
+def get_page():
     return BeautifulSoup(requests.get('https://teamtrees.org').content, 'html.parser')
 
 
 @app.route('/', subdomain='api')
 def get_trees():
-    page = getPage()
+    page = get_page()
     treecount = page.select_one('#totalTrees')['data-count']
     return treecount
 
 
 @app.route('/donations', subdomain='api')
 def get_donations():
-    page = getPage()
+    page = get_page()
     donations_tag = page.select(('#top-donations' if bool(request.args.get('top') == 'True') else '#recent-donations') + ' .media')
     donations = []
     for donation in donations_tag:
@@ -41,26 +41,43 @@ def get_donations():
         })
     return json.dumps(donations)
 
-@app.route('/hooks/add', subdomain='api', methods=['POST'])
+@app.route('/hooks', subdomain='api', methods=['POST', 'DELETE'])
 def create_hook():
-    hook = {
-        "url": request.args.get('url'),
-        "delay": int(request.args.get('delay')),
-        "top": request.args.get('top')
-    }
-    threading.Thread(target=hook_thread, args=[hook]).start()
-    return "Thread created!"
+    if request.method == 'POST':
+        hook = {
+            "url": request.args.get('url'),
+            "delay": int(request.args.get('delay')),
+            "top": request.args.get('top'),
+            "token": token_urlsafe(24),
+            "active": True
+        }
+        thread = threading.Thread(target=hook_thread, args=[hook])
+        thread.start()
+        hooks.append((hook, thread))
+        write_hooks()
+        return hook['token']
+    if request.method == 'DELETE':
+        try:
+            index = [hook['token'] for hook in [hook[0] for hook in hooks]].index(request.args.get('token'))
+            hooks[index][0]['active'] = False
+            hooks[index][1].join()
+            del hooks[index]
+            write_hooks()
+            return "Deleted " + request.args.get('token')
+        except:
+            return "Couldn't find token " + request.args.get('token')
+
 
 
 @app.route('/')
 def index():
-    return 'Home'
+    return "Home"
 
 
 def hook_thread(hook):
     request_url = f'http://api.{app.config["SERVER_NAME"]}/donations?top={hook["top"]}'
     previous_donations = requests.get(request_url).text
-    while(True):
+    while(hook['active']):
         time.sleep(hook['delay'])
         new_donations = requests.get(request_url).text
         
@@ -81,3 +98,8 @@ def hook_thread(hook):
 
 if __name__ == "__main__":
     threading.Thread(target=app.run).start()
+    with open('hooks.json') as file:
+        for hook in json.load(file):
+            thread = threading.Thread(target=hook_thread, args=[hook])
+            thread.start()
+            hooks.append((hook, thread))
