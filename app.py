c2f211583cf55fd91b03f6cf0b660455dad1e0c4
Seth Painter <timytomy11@gmail.com>
2019-11-11 18:21:55 -0600

    added an endpoint to return the donations

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import request
 from bs4 import BeautifulSoup
 import requests
+import json
+
 
 app = Flask(__name__, subdomain_matching=True)
 
@@ -10,9 +13,23 @@ def getPage():
 @app.route('/', subdomain='api')
 def get_trees():
     page = getPage()
-    treecount = page.select('#totalTrees')[0]['data-count']
+    treecount = page.select_one('#totalTrees')['data-count']
     return treecount
 
+
+@app.route('/donations', subdomain='api')
+def get_new_doners():
+    page = getPage()
+    donations_tag = page.select(('#top-donations' if request.args.get('top') else '#recent-donations') + ' .media')
+    print(donations_tag)
+    donations = []
+    for donation in donations_tag:
+        donations.append({
+            'name': donation.select_one("strong").get_text(),
+            'treeCount': int(donation.select_one(".feed-tree-count").get_text().split(" ")[0].replace(',', '')),
+            'message': donation.select_one(".medium").get_text()
+        })
+    return json.dumps(donations)
 
 @app.route('/')
 def index():
